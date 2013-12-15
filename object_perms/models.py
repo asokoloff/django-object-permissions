58fb5c5b6da9c3d21203bc0f377e52741e37143f
asokoloff <info@alexsokoloff.net>
2013-12-15 12:35:07 -0500

    Rewrite that changes method of determining permission inheritance among objects. Adds mapping table PermissionAncestor. Adds methods to 1) determine permissions using joins to PermissionAncestor and 2) to populate PermissionAncestor as needed when PermissionableObjects are saved. Test are now broken. :(

--- object_perms/models.py
+++ object_perms/models.py
@@ -1,6 +1,5 @@
-from django.db import models
-from django.db.models import Q
-import operator
+from django.db import models, transaction
+from model_utils.managers import InheritanceManager
 
 PRIVILEGES = (
     ('admin', 'Administer'),
@@ -10,19 +9,34 @@ PRIVILEGES = (
 )
 
 class Party(models.Model):
-    pass
 
+    # does this make sense in the context of a subclass?
+    def get_memberships(self, require_permissions_inherit=True):
+        """
+        recursively identify group memberships
+        """
+        result = [self]
+        for item in self.memberships:
+            if item.inherit_permissions or not require_permissions_inherit:
+                result.extend(item.member_of.get_memberships())
+        return result
+
+    def get_direct_permissions(self, privilege):
+        PermissionableObject.objects.filter(
+            partyprivilege__party__in=self.get_memberships(),
+            partyprivilege__privilege=privilege
+            )
 
 class Membership(models.Model):
 
     member = models.ForeignKey(
         'Party',
-        related_name='members'
+        related_name='memberships'
         )
 
     member_of = models.ForeignKey(
         'Party',
-        related_name='memberships'
+        related_name='members'
         )
 
     inherit_permissions = models.BooleanField(default=False)
@@ -31,54 +45,103 @@ class Membership(models.Model):
 class PermissionableObject(models.Model):
 
     permission_parent_classes = list()
+    objects = InheritanceManager()
+
+    def __unicode__(self):
+        return "id: {0}".format(self.id)
 
     @classmethod
     def get_permitted_items(cls, party, privilege):
 
-        # TODO: add the ability to figure out memberships
+        direct_permissions = party.get_direct_permissions(privilege)
 
-        dpos = PermissionableObject.objects.filter(
-            partyprivilege__party=party,
-            partyprivilege__privilege=privilege
-            ).values_list('id',flat=True)
-
-        return cls._get_descendant_objects(dpos)
+        return cls.objects.filter(
+            permissionableobject_ptr__permission_ancestors__ancestor_object__in=direct_permissions
+            ).distinct()
 
     @classmethod
-    def _get_descendant_objects(cls, directly_permed_objects):
-        """ 
-        Expects a list of permissionableobject ids over which the user has
-        direct privileges. Builds a query that searches for matches in
-        the curent model or any model the current model inherits
-        permissions from using Q objects.  Returns a query set of
-        objects over which the user has either direct or inherited
-        privileges.
+    def _get_ancestor_paths(cls):
         """
-        q_params = list()
-        q_params.append('permissionableobject_ptr_id__in')
-        for p_class in cls.permission_parent_classes:
-            q_params.extend(
-                [x + '__permissionableobject_ptr_id__in' for x in \
-                                 p_class._get_parent_privilege_paths()]
-                )
-        query_predicates = [(x, directly_permed_objects) for x in q_params]
-        q_list = [Q(x) for x in query_predicates]
-        return cls.objects.filter(reduce(operator.or_, q_list))
-
-    @classmethod
-    def _get_parent_privilege_paths(cls):
-        """
-        Builds a set of paths to parent objects for permission query
-        builder.
+        Recursively adds paths to parent objects when calculating
+        permission ancestors.
         """
         all_paths = list()
+        this_segment = cls.__name__.lower()
         for x in cls.permission_parent_classes:
-            for path in x._get_parent_privilege_paths():
-                all_paths.append(cls.__name__.lower() + '__' + path)
-        all_paths.append(cls.__name__.lower())
+            for path in x._get_ancestor_paths():
+                all_paths.append('{0}.{1}'.format(this_segment, path))
+        all_paths.append(this_segment)
         return all_paths                            
 
-    
+
+    def _derive_permission_ancestors(self):
+        """
+        Calculates objects that the present object inherits
+        permissions from using fk relations in subclasses. Returns
+        list of instances of PermissionAncestor (not subclass).
+        """
+        ancestor_str = list()
+        for x in self.__class__.permission_parent_classes:
+            for path in x._get_ancestor_paths():
+                ancestor_str.append('self.{0}.permissionableobject_ptr'.format(path))
+        ancestor_str.append('self.permissionableobject_ptr')
+        return [eval(x) for x in ancestor_str]
+
+    def _update_permission_ancestor_data(self):
+
+        derived_ancestors = self._derive_permission_ancestors()
+        stored_ancestors = PermissionAncestor.objects.filter(child_object=self)
+
+        # check if anything has changed
+        if set([x.id for x in derived_ancestors]) == set([x.ancestor_object_id for x in stored_ancestors]):
+            return
+
+        # FIXME: add transaction handling
+        PermissionAncestor.objects.filter(child_object=self).delete()
+        for ancestor in derived_ancestors:
+            PermissionAncestor.objects.create(child_object=self,ancestor_object=ancestor)
+
+        self._update_childrens_ancestor_data()
+
+    def _update_childrens_ancestor_data(self):
+        """
+        Uses PermissionAncestor to find child
+        objects. InheritanceManager (select_subclasses queryset
+        method) returns subclass instances that we need.
+        """
+        permission_children = PermissionableObject.objects.filter(
+            permission_ancestors__ancestor_object=self
+            ).select_subclasses()
+        for child in permission_children:
+            child._update_permission_ancestor_data()
+
+#     def save(self, *args, **kwargs):
+#         print "about to save instance of", self.__class__
+#         super(self.__class__, self).save(*args, **kwargs)
+
+
+class PermissionAncestor(models.Model):
+    """
+    For a given permissionable object, this table records all of the
+    other permissionable objects from which the first object inherits
+    permissions, including the object itself. (Each permissionable
+    object is treated as its own object_ancestor in this table.) The
+    purpose of storing the data in this form is to allow simple
+    queries that traverse object hierarchies of any depth. The data in
+    this table is derived using the foreign key relationships of
+    tables that subclass PermissionableObject. The table is kept
+    current by the save method for PermissionableObject.
+    """
+
+    child_object = models.ForeignKey(PermissionableObject,
+                                              related_name='permission_ancestors')
+
+    ancestor_object = models.ForeignKey(PermissionableObject,
+                                        related_name='permission_children')
+
+    class Meta:
+        unique_together = ('child_object', 'ancestor_object')
+
 class PartyPrivilege(models.Model):
     
     party = models.ForeignKey(Party)
@@ -89,6 +152,6 @@ class PartyPrivilege(models.Model):
         )
 
     class Meta:
-        unique_together = ('party', 'permissionable_object')
+        unique_together = ('party', 'permissionable_object', 'privilege')
 
 
